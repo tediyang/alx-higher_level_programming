7f07d1ee2fa29d7acdbe9196e6c7c1ab50418f8f
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-11-30 01:54:40 +0100

    concat edges edited

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = str1[39:67] + str1[-22:-17] + str1[:7]
-print(str1)
+str = str[39:67] + str[-22:-17] + str[:7]
+print(str)
