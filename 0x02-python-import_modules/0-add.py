451ed429b377ae92a4917b7b67434749856fead5
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-01 13:32:20 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+from add_0
 
 a = 1
 b = 2
 
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
