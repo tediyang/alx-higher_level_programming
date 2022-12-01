9676a81bfa7d33d3a9a3ba1dc039496d7ae97ab0
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-01 06:40:36 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     elif num == 2:
         print(f"{num - 1} argument:\n1: {sys.argv[1]}")
     else:
-        print(f"{num - 1} arguments")
+        print(f"{num - 1} arguments:")
         for i in range(1, num):
             print(f"{i}: {sys.argv[i]}")
