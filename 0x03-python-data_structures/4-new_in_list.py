70294c02a39f80e1d7499f15565af102446513d4
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-05 10:40:26 -0800

    new in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+
     if my_list is None:
         return
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     new = my_list.copy()
     new[idx] = element
 
