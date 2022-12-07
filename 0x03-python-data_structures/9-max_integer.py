6a418721870b840a59cb7056c9a17c26efa0d2da
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-07 06:53:28 -0800

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    large = 0
+    for i in my_list:
+        if i > large:
+            large = i
+
+    return large
