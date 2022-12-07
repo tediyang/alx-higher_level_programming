aee3f6ef5fd610ccce2a2e399d714aaf0b30423e
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-07 05:18:22 -0800

    add tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a or not tuple_b:
+        return tuple_a or tuple_b
+
+    a, b = 0, 0
+    if tuple_a < 2 or tuple_b < 2:
+        if tuple_a < 2:
+            a += tuple_a[0] + tuple_b[0]
+            b += tuple_b[1]
+        else:
+            a += tuple_a[a] + tuple_b[0]
+            b += tuple_a[1]
+
+    else:
+        a += tuple_a[0] + tuple_b[0]
+        b += tuple_a[1] + tuple_b[1]
+
+    return a, b
