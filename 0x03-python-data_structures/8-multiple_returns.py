a90701e291164c5d196e76779bd0090ff7409c6d
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-07 06:41:33 -0800

    multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        return len(sentence), None
+    if len(sentence) == 0:
+        return len(sentence), None)
     return len(sentence), sentence[0]
