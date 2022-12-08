a4d041f1957e7f7038899b0b60736f173566da3f
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-08 01:49:04 -0800

    uncommon

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    if len(set_1) == 0 or len(set_2) == 0:
+        return list(set_1) or list(set_2)
+
+    uncommon = [i for i in set_1 if i not in set_2]
+    return uncommon
