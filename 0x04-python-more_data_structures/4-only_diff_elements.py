7875d9ccce39a97c41bd79efb7f27c5ba00f2b93
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-08 01:57:30 -0800

    uncommon

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,5 @@ def only_diff_elements(set_1, set_2):
         return list(set_1) or list(set_2)
 
     uncommon = [i for i in set_1 if i not in set_2]
+    uncommon.extend([i for i in set_2 if i not in set_1])
     return uncommon
