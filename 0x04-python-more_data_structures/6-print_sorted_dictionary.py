de099a9ec07c90628691a1f06add1b50e4efb316
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-09 21:16:57 +0100

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sortrd_dictionary(a_dictionary):
     new = sorted(a_dictionary)
 
     for i in new:
-        print(f"{i}: {a_dictionary[i]}")
+        print(f"{i}: {a_dictionary.get(i)}")
