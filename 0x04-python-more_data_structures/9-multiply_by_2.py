7fcb8fa6257bf6d9b92f82912883e762af363e67
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-11 05:11:19 -0800

    multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for key, value in a_dictionary.items():
-        a_dictionary[key] = value * 2
+    new = a_dictionary.copy()
+    for key, value in new.items():
+        new[key] = value * 2
 
-    return a_dictionary
-
+    return new
