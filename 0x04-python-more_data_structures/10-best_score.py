1e2a457c2b6d2efbb28d1fd9a521c79c9d29a3df
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-11 05:35:34 -0800

    map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     return max(a_dictionary.keys())
