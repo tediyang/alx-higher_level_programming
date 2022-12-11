28a7e7e2a69f6426c551bfbf342d722048425b46
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-11 05:00:50 -0800

    delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is None:
         del a_dictionary[key]
 
-        return a_dictionary
+    return a_dictionary
