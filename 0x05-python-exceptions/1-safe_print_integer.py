ea586b07a0627fb3656b42e8bbf7550d0ea606d1
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-20 15:09:53 +0100

    safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@
 def safe_print_integer(value):
     try:
         if value < 0:
-            b = str(abs(value))
-            for i, v in enumerate(b):
+            for i, v in enumerate(str(abs(value))):
                 if i == 0:
                     print("{:d}".format(int(v) * -1), end='')
                 else:
