1e89ec204bbee91f870227978c5534eba3908125
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-20 07:13:36 -0800

    print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         c = None
         return None
-    finally
-    print("Inside result: {}".format(c))
+    finally:
+        print("Inside result: {}".format(c))
