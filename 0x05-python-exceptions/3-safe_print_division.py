657086b558c26bc03ccb26d4bc2a3060896b3fac
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-20 07:12:02 -0800

    print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,6 +2,10 @@
 
 def safe_print_division(a, b):
     try:
-        return a / b
+        c = a / b
+        return c
     except ZeroDivisionError:
+        c = None
         return None
+    finally
+    print("Inside result: {}".format(c))
