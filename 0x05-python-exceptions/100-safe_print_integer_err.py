a792612028fc9789db939b0da7e7e43095db8ac6
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2022-12-20 08:58:41 -0800

    100 task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as val:
+        print("Exception: {}".format(val))
+        return False
