a4043438d79fd44d3a443d26efa8f8fe7d2e94b1
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-27 22:33:14 +0100

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Class Square: This class uses a private
+    attributes called size and calculates the
+    area of the square/.
+"""
+
+
+class Square:
+    """Class constructor"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        
+    def area(self):
+        return self.__size **2
