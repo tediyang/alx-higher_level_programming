22e52d70374061aa0d5e3346af6ce933b4033c3e
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-27 23:41:38 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,5 +31,5 @@ class Square:
         if self.size == 0:
             print()
         else:
-            for _ in range(self.size):
+            for i in range(self.size):
                 print('#' * self.size)
