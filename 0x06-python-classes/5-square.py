b830102195c502896fbd4a263deb25153863d9d8
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2022-12-27 23:42:33 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,35 +1,45 @@
 #!/usr/bin/python3
 """ Class Square that defines a square by
     Private instance attribute: size
+    Getter and Setters
     Instantiation with optional size
-    size must be an integer. Get the area 
-    and print the result.
+    size must be an integer
+    Public instance method: def area(self)
+    Public instance method: def my_print(self)
 """
 
 
 class Square:
     """Class constructor"""
     def __init__(self, size=0):
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, size):
         if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
 
-    def area(self):
-        return self.size ** 2
+    """Size getter"""
+    @property
+    def size(self):
+        return self.__size
 
+    """Size setter"""
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    """returns the current square area"""
+    def area(self):
+        return self.__size ** 2
+
+    """prints in stdout the square with the character #"""
     def my_print(self):
-        if self.size == 0:
-            print()
-        else:
-            for i in range(self.size):
+        if self.size != 0:
+            for ch in range(self.size):
                 print('#' * self.size)
+        else:
+            print()
