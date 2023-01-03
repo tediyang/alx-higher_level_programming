4a5b13d23d2079b744223967d1b01a68ee0ca2e0
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-03 23:43:57 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Constructor method """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
