6c02c29b28217650ed9a2c7b996e7a74bbf8e1cd
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-04 00:28:42 +0100

    Create 3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''Rectangle Module'''
+
+
+class Rectangle:
+    '''Rectangle object with perimeter and area'''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.height == 0 or self.width == 0:
+            return 0
+        return 2 * (self.width + self.height)
+      
+    def __str__(self):
+        """ Return string to print rectangle with # """
+        if self.width == 0 or self.height == 0:
+            return ''
+        to_print = ''
+        for col in range(self.height):
+            for row in range(self.width):
+                to_print += '#'
+            if col != self.height - 1:
+                to_print += '\n'
+        return to_print
