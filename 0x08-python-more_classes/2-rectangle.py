efcbdde14da17cee8335493bc1627128006fd2a9
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-03 15:04:00 -0800

    perimeter and area

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,9 +17,9 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError('width must be an integer')
-        if width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -27,11 +27,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(height) != int:
+        if type(value) != int:
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
-        self.__height = height
+        self.__height = value
 
     def area(self):
         return self.width * self.height
