85109f8c1a612bbab7934b8d525523efbe18aad7
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-03 14:20:39 -0800

    rectangle private attribute

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
 
     @width.setter
     def width(self, width):
-        if isinstance(width, int):
+        if not isinstance(width, int):
             raise TypeError('width must be an integer')
         if width < 0:
             raise ValueError('width must be >= 0')
@@ -25,7 +25,7 @@ class Rectangle:
 
     @height.setter
     def height(self, height):
-        if isinstance(height, int):
+        if not isinstance(height, int):
             raise TypeError('height must be an integer')
         if height < 0:
             raise ValueError('height must be >= 0')
