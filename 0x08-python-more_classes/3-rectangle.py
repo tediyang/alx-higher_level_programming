d7c7e67c98ef6bd37c4b6f4f40cea314a3ccb9c0
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-04 01:59:37 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
         if self.height == 0 or self.width == 0:
             return 0
         return 2 * (self.width + self.height)
-      
+
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ''
