3aec53ce97e9436e4244bc0d880679caa67283fe
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-04 03:08:50 +0100

    Create 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Text indentation function'''
+
+
+def text_indentation(text):
+    if text is None:
+        raise TypeError("text_indentation() missing 1 required positional argument: 'text'")
+
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    for i, c in enumerate(text):
+        if c in ['.', '?', ':']:
+            print(c, end='\n')
+            continue
+        if c == ' ' and i != 0 and text[i - 1] in ['.', '?', ':']:
+            continue
+        print(c, end='')
