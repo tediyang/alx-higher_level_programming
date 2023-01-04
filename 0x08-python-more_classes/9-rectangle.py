b68ec2d4509a8bc95654eebc91a8a0e6cdf7aad9
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-04 02:05:02 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
         """ Return a string when an attribute is deleted """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
