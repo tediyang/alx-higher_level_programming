d7d7691a9ef5f9dc1fdaacec810be5828709c010
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-16 15:41:16 +0100

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,6 +14,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         '''accepts two parameters and validate the value arg'''
         if not isinstance(value, int):
-            raise TypeError(f'{name} must be integer')
+            raise TypeError(f'{name} must be an integer')
         if value <= 0:
             raise ValueError(f'{name} must be greater than 0')
