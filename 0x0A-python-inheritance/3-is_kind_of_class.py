590aae4d981dd9492d46916b737e27e17f23d56f
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 05:26:37 -0800

    subclass

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-  '''Check if obj is a subclass of a_class'''
-  return issubclass(obj, a_class)
+    '''Check if obj is a subclass of a_class'''
+    return issubclass(obj, a_class)
