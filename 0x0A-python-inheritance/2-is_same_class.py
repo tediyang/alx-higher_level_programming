33617fa020a83e510886abac77c0bb00585dbbf1
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 05:11:34 -0800

    same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,4 @@
 
 def is_same_class(obj, a_class):
     '''check if they are the same class'''
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return type(obj) == a_class
