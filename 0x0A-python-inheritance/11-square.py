f8a7b4fca40e7e39cb06b7cb044c7b825b7d0efd
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-16 17:35:10 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 '''Square Module'''
+Rectangle = __import__("9-rectangle").Rectangle
 
+
+class Square(Rectangle):
+    '''class module inherits from rectangle'''
+    def __init__(self, size):
+        '''initialization'''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''return the area of a square'''
+        return self.__size ** 2
+      
+    def __str__(self):
+        '''prints the module and the parameters'''
+        return f"[Square] {self.__size}/{self.__size}"
