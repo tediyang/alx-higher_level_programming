717b2358d56ff9bf196d5e8fbb75e13d08f4f505
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 05:37:56 -0800

    inherit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''check if obj inherits from a_class'''
-    return isinstance(obj, a_class) and type(obj) == a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
