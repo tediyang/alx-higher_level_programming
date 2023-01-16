36eb6b5724f47d763863d67d2c364c1740049173
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 08:24:59 -0800

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Square Module'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    '''class module inherits from rectangle'''
+    def __init__(self, size):
+        '''initialization'''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''return the area of a square'''
+        return self.__size ** 2
+
+    def __str__(self):
+        '''return the model name and parameter'''
+        return f"[Rectangle] {self.__size}/{self.__size}"
