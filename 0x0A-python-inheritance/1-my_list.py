7d09cfdbc1fd7d3ad4985b0d28459a9c3e7ea339
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-16 13:50:43 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-'''Mylist module'''
+"""MyList module"""
 
 
-class Mylist(list):
-    """Mylist class - Inherit from list"""
+class MyList(list):
+    """MyList class - Inherits from list"""
     def print_sorted(self):
-        """print sorted list"""
+        """Prints a sorted list"""
         print(sorted(self))
