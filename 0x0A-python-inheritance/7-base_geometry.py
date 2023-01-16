a9b05503cbc56fcf35b1b5063ea4368f1735c5cb
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 06:32:20 -0800

    7 task

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''BaseGeometry Module'''
+
+
+class BaseGeometry:
+    '''
+        class BaseGeometry with 2 public instances.
+        area() raises an exception.
+        integer_validator() validates an integer.
+    '''
+    def area(self):
+        '''raises an exception'''
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        '''accepts two parameters and validate the value arg'''
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
