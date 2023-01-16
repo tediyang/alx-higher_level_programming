4d292143b83b6f0115c0e1937607077908de5a72
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-16 06:12:14 -0800

    area exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''BaseGeometry Module'''
+
+
+class BaseGeometry:
+    '''class geometry with one public instance'''
+    def area(self):
+        '''raise an exception'''
+        raise Exception('area() is not implemented')
