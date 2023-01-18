1295aa4a1cd8742799a25e7c002855b572e4d7a5
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-01-17 16:17:52 -0800

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Base Model'''
+
+
+class Base:
+    '''Base class'''
+    __nb_objects = 0
+    def __init__(self, id=None):
+        '''Initialization'''
+        self.id = id
+        if self.id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
