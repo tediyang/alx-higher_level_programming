58c562c3ff4d55dbc87bd3be827fb3db3ec83204
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-01-19 21:19:23 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,4 +10,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.height}'
+        return f'[Square] ({self.id}) {self.x}/{self.y} \
+- {self.height}'
