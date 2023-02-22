ba8279f56be8cdc5831fbf8660ab0f7a67722af2
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-02-22 18:22:11 +0100

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- Query that display cities with high temp during july and aug
 SELECT city, AVG(value) avg_temp
 FROM temperatures
-WHERE month BETWEEN 7 AND 8
+WHERE month BETWEEN 6 AND 9
 GROUP BY 1,
 ORDER BY 2 DESC
 LIMIT 3;
