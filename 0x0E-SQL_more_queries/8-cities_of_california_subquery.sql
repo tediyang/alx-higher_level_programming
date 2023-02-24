bdb074680b00f23dd8725cbbf68a2f9d573dd351
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-02-24 21:42:38 +0100

    Create 8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- cities of california
+-- Query that prints the cities of california
+SELECT id, name
+FROM cities
+WHERE state_id =
+      (SELECT id
+      FROM states
+      WHERE name = 'California');
