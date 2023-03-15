f832d1d63ef105f0a13077f261789782531fd549
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-15 23:34:08 +0000

    multi c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,10 +6,10 @@ for (len = 0; process.argv[len];) {
 if (len === 2) {
   console.log('Missing number of occurrences');
 } else {
-  const num =  parseInt(process.argv[2], 10);
+  const num = parseInt(process.argv[2], 10);
   if (num >= 0) {
     for (let i = 0; i < num; i++) {
-      console.log('C is fun')
+      console.log('C is fun');
     }
   }
 }
