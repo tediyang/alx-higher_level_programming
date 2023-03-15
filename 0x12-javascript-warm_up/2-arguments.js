39ee02351036bf024fa5427a75df992e59d562ba
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-15 19:08:29 +0000

    2-arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const len = process.argv.length;
 if (len === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (len === 3) {
   console.log('Argument found');
 } else {
