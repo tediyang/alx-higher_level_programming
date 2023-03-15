6924a50c1a9f3d208ee10f73880329e8c3d6824c
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-15 22:59:41 +0000

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+let len;
+for (len = 0; process.argv[len];) {
+  len++;
+}
+if (len === 2) {
+  console.log('Not a number');
+} else {
+  const digit = parseInt(process.argv[2], 10);
+  if (Number.isInteger(digit)) {
+    console.log(digit);
+  } else {
+    console.log('Not a number');
+  }
+}
