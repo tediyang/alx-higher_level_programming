81595209e55462efccce736664f55b20cc9dc626
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-15 19:38:31 +0000

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-const len = process.argv.length;
+let len;
+for (len = 0; process.argv[len]; len++) {
+  ;
+}
 if (len === 2) {
   console.log('No argument');
 } else {
-  console.log(process.argv[2);
+  console.log(process.argv[2]);
 }
