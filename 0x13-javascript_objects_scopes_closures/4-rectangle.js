ff71fe83f8c74fcf3bf80bb4e4d2c189cbf50f53
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-19 09:41:49 +0000

    rectangle 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = 'X';
+      for (let j = 1; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+}
+
+module.exports = Rectangle;
