ff1864ec822489794a0d5ef85d5db494d7cf39f7
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-24 10:03:18 -0700

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Import needed modules """
+import sys
+import MySQLdb as engine
+
+
+def main(argv):
+    """ Access database and print states with name starting
+        with N.
+    """
+    db = engine.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
