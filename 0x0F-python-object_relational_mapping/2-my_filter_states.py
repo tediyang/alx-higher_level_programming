d53f4d903de33961cad1f69cbfca25719c3e6f42
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-24 10:14:35 -0700

    using user arg to filter state

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Import needed modules """
+import sys
+import MySQLdb as engine
+
+
+def main(argv):
+    """ Access database and print states with name starting
+        with N.
+    """
+    db = engine.connect(host="localhost", port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY id"
+                .format(argv[4]))
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
