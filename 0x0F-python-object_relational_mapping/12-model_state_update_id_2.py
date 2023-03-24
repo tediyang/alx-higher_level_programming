856f56d8c6661dc8e9363a63621c0fe4be40bf04
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-24 16:03:51 -0700

    model state update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Import modules"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import create_engine
+
+
+def main(argv):
+    """ create the table in the database and print
+        the first instance.
+    """
+    URL = f"""mysql+mysqldb://{argv[1]}:{argv[2]}
+            @localhost/{argv[3]}"""
+    engine = create_engine(URL, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for row in session.query(State).filter(State.id == 2):
+        row.name = "New Mexico"
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
