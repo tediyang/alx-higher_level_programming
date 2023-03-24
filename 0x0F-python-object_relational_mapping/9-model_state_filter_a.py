ab45c48b2dda8dc08bc9cbc147e1dfe62fc3fd84
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-24 14:09:52 -0700

    model statw filter by a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Import modules"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import create_engine
+
+
+def main(argv):
+    """ create the table in the database and print
+        the first instance.
+    """
+    URL = f"""mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}
+            @localhost/{sys.argv[3]}"""
+    engine = create_engine(URL, pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
+        print(row)
+    session.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
