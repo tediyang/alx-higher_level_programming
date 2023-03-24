282bb501c9c38b5fc75bc5f1c6e29375ae7c5d7c
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-03-24 09:47:23 -0700

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 
 def main(argv):
     """ Access database and print all the states. """
-    db = MySQLdb.connect(host="3306", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
