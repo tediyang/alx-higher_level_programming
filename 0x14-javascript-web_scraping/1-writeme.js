e9e2a184d1a025f903a48f066a86cdccfa98e3e7
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-03 23:10:33 +0100

    1-writeme.js semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const filename = process.argv[2];
 const words = process.argv[3];
-fs.writeFile(filename, words, "utf-8", err => {
-    if (err) throw err;
+fs.writeFile(filename, words, 'utf-8', err => {
+  if (err) throw err;
 });
