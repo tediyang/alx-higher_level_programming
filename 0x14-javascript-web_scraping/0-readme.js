d97dc407cc329d6105888cb366641d6a6acd8a3b
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-03 22:40:16 +0100

    0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-
-if (process.argv.length < 3) {
-  console.log('Usage: ./0-readme.js Filename');
-  process.exit(1);
-}
-
 // Read the file and print its contents.
 const fs = require('fs');
 const filename = process.argv[2];
