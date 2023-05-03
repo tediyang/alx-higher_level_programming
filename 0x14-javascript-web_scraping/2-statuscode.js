7c42e0bdffea9264df193603d7fd9a13b4d8f301
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-03 23:36:51 +0100

    2-statuscode.js semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // Get status code.
 const request = require('request');
 const URL = process.argv[2];
-request (URL, (err, res, body) => {
+request(URL, (err, res, body) => {
   if (err) throw err;
   console.log(`Code: ${res.statuscode}`);
-})
+});
