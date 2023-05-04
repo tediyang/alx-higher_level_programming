c71074eb6b67173fc7559fff58edb957df2a0d7c
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-04 15:09:11 +0100

    documentation for 5-request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,6 +6,13 @@ const request = require('request');
 const fs = require('fs');
 const URL = process.argv[2];
 const filename = process.argv[3];
+
+/*
+  Step 1: Create a GET request.
+  Step 2: Throw the error if one occurred.
+  Step 3: Write to the file, the body of the webpage
+  Step 4: Throw error if one occurred.
+*/
 request(URL, (err, res, body) => {
   if (err) throw err;
   fs.writeFile(filename, body, 'utf-8', err => {
