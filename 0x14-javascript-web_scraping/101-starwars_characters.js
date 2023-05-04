ddf6f6132868fdf28114cc5bb5adb0a6b2f3d3b5
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-04 17:51:37 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -11,13 +11,15 @@ const URL = `https://swapi-api.alx-tools.com/api/films/${id}`;
   Step 2: Throw the error if one occurred.
   Step 3: Get the results in json format,
   getting all the characters from the film requested.
-  Step 4: loop through every character link and get the name of the char.
+  Step 4: loop through the film characters and get the index.
+  Step 5: using the index to get the character link.
   Step 5: Print the characters.
 */
 request(URL, (error, response, body) => {
   if (error) throw error;
   const filmChars = JSON.parse(body).characters;
-  for (const character of filmChars) {
+  for (const index in filmChars) {
+    const character = filmChars[index];
     request(character, (err, res, bdy) => {
       if (err) throw err;
       console.log(JSON.parse(bdy).name);
