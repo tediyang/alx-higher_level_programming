b071fbb7a9e24bf932aa93a6fa312bbcae74d854
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-04 15:07:25 +0100

    documentation for 4-starwars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,6 +5,15 @@
 */
 const request = require('request');
 const URL = process.argv[2];
+/*
+  Step 1: Create a GET request.
+  Step 2: Throw the error if one occurred.
+  Step 3: Get the results in json format.
+  Step 4: Get all the characters from each film. {[characters], [characters]..}
+  Step 5: Map and Filter each list of characters by cheacking for ID 18.
+  Step 6: Remove all empty list with no found values.
+  Step 7: Print the length.
+*/
 request(URL, (err, res, body) => {
   if (err) throw err;
   const films = JSON.parse(body).results
