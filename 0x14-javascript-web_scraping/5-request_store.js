115062ec92ceda1475db3f19beb41fdd7bdea685
Eyang Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-04 13:57:50 +0000

    executable

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,7 +8,7 @@ const URL = process.argv[2];
 const filename = process.argv[3];
 request(URL, (err, res, body) => {
   if (err) throw err;
-  fs.writeFile(filename, body.text, 'utf-8', err => {
+  fs.writeFile(filename, body, 'utf-8', err => {
     if (err) throw err;
   });
 });
