c12a39c56f65e50f0a1b0fbcda95383dc05788c9
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-04 15:31:39 +0100

    documentation for 1-writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 // Write to a file.
+
+/*
+  Step 1: Create a file module.
+  Step 2: Create the filename.
+  Step 3: words to write.
+  Step 3: Write the words into the file.
+  Step 4: Throw error if found.
+*/
 const fs = require('fs');
 const filename = process.argv[2];
 const words = process.argv[3];
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 // Get status code.
+
+/*
+  Step 1: Create a request module.
+  Step 2: Create the url link.
+  Step 3: Create a GET request.
+  Step 4: Throw error if found.
+  Step 5: Print the response statusCode.
+*/
 const request = require('request');
 const URL = process.argv[2];
 request(URL, (err, res, body) => {
