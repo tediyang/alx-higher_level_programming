f6acc8842a292ed558da05524366d63e6df4c08d
Eyang Daniel Eyoh <61313270+tediyang@users.noreply.github.com>
2023-05-04 18:06:31 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,31 +1,21 @@
 #!/usr/bin/node
-/*
-  A script that prints all characters of a Star Wars movie,
-  according to the starring.
-*/
+
 const request = require('request');
-const id = process.argv[2];
-const URL = `https://swapi-api.alx-tools.com/api/films/${id}`;
-/*
-  Step 1: Create a GET request.
-  Step 2: Throw the error if one occurred.
-  Step 3: Get the results in json format,
-  getting all the characters from the film requested.
-  Step 4: loop through the film characters and get the index.
-  Step 5: using the index to get the character link.
-  Step 5: Print the characters.
-*/
-request(URL, (error, response, body) => {
-  if (error) throw error;
-  const filmChars = JSON.parse(body).characters;
-  const max_index = filmChars.length;
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
+request(url, (err, res, body) => {
+  if (err) console.log(err);
   const index = 0;
-  while (index < max_index) {
-    const character = filmChars[index];
-    request(character, (err, res, bdy) => {
-      if (err) throw err;
-      console.log(JSON.parse(bdy).name);
-    });
-    index++;
-  }
+  const characters = JSON.parse(body).characters;
+  printCharcter(characters, index);
 });
+
+const printCharcter = function (url, i) {
+  request(url[i], (err, res, body) => {
+    if (err) console.log(err);
+    console.log(JSON.parse(body).name);
+    if (++i < url.length) {
+      printCharcter(url, i++);
+    }
+  });
+};
