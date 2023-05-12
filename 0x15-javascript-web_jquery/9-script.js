4d27b906df09a2c553596dee73b50473d1cd96d6
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-12 17:49:01 +0100

    javascript loaded from head 9-script.js

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,22 @@
+$(document).addEventListener('DOMContentLoaded', () => {
+    const $hello = $('div#hello');
+    
+    $.ajax({
+        type: "GET",
+        url: "https://fourtonfish.com/hellosalut/?lang=fr",
+        success: data => {
+            $('div#hello').text(data.hello);
+        },
+
+        error: () => {
+            console.log("Couldn't get data");
+        }
+    });
+    
+});
+
+$('document').ready(() => {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', data => {
+      $('div#hello').text(data.hello);
+    });
+  });
